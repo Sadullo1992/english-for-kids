a2e3b0cedae0f7af360d88310079010502486c08
Sadullo <sadulloburiyev1992@gmail.com>
2022-07-13 22:39:38 +0500

    fix: fix SetStatistic and some css style

--- js/statistic/setStatistic.js
+++ js/statistic/setStatistic.js
@@ -5,12 +5,14 @@ export default function SetStatistic(wordObj, mode) {
             if(item.word === wordObj.word && item.translation === wordObj.translation) {
                 if(mode === 'train') {
                     item.train++;
-                } else if(mode === 'play'){
-                    item.play++;                                    
                 } else {
-                    item.wrong++;   
+                    if(mode === 'play'){
+                        item.play++;                                    
+                    } else {
+                        item.wrong++;   
+                    }                
+                    item.error = Math.round((item.wrong / item.play) * 100);
                 }                
-                item.error = Math.round((item.wrong / item.play) * 100);
                 localStorage.setItem('statisticData', JSON.stringify(statisticData));
             }
         });
